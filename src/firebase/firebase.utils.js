aa334335ed2dbb9b91fd6e9b525e3026ebf49c36
jessie <wujiaojiao82@gmail.com>
2021-01-26 13:31:34 +0800

    implement authentication via google firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,9 +11,37 @@ const firebaseConfig = {
     messagingSenderId: "301633276871",
     appId: "1:301633276871:web:c2dff2c975e9024ceee7e2",
     measurementId: "G-LML5JRG9EG"
-  };
+};
+
 
 firebase.initializeApp(firebaseConfig);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  
+  const userRef = firestore.doc(`/users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+
+}
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
@@ -23,5 +51,4 @@ provider.setCustomParameters({promt: 'select_account'});
 export const signInWithGoogle = ()=> auth.signInWithPopup(provider);
 
 export default firebase;
-
 
